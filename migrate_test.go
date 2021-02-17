933c8f23972f11bbe5d017f199722b551c1b7dfc
Law Zava <law@zava.dev>
2021-02-17 12:10:48 +0200

    :sparkles: custom info logger support

--- migrate_test.go
+++ migrate_test.go
@@ -162,6 +162,10 @@ func performMigrateTaskWithMigrations(t *testing.T, repo repository, options Opt
 func performMigrateTask(t *testing.T, repo repository, options Options, migrations []*Migration) error {
 	t.Helper()
 
+	options.LogInfo = func(format string, args ...interface{}) {
+		fmt.Printf(format+"\n", args...)
+	}
+
 	task := migrationTask{
 		migrations: mapMigrations(migrations),
 		repo:       repo,
